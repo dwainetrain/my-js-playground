c909af9fafd75e0ce5416c87fb095dec0bf97ea8
Science Illustration <scienceillustration@Dwaine-Bests-MacBook-Pro-2.local>
2020-02-02 14:35:02 -0800

    struggling with prime number idea

--- fcc/fib.js
+++ fcc/fib.js
@@ -1,36 +1,81 @@
-function sumFibs(num) {
-    // okay, just a loop
-    function fibSeq(num) {
-        let fibColl = [];
-        let a = 1, b = 0, temp;
-
-        while (num >= 0) {
-            temp = a;
-            a = a + b;
-            fibColl.push(b);
-            b = temp;
-            num--;
-        }
-        return fibColl;
-
+function sumPrimes(num) {
+    let range = [];
+    for (let i = 1; i <= num; i++) {
+        range.push(i);
     }
 
-    let fibArr = fibSeq(num);
+
+    let zeroRange = range.map(number => range.map(divider => number%divider).filter(item => item < 1) )
+
+    console.log(zeroRange);
+    console.log( zeroRange.filter(arr => arr.length <= 2) );
+
+    let primeArr = []
+    for (let i in zeroRange) {
+        primeArr.push(range[i]);
+    }
+
+
+    return primeArr;
+  }
+  
+  console.log( sumPrimes(10) );
+
+ // 1(1/1), 2(1/2), 3(1/3), 4(2/2), 5(1/5), 6(2/3), 7(1/7), 8(2/2/2), 9(3/3), 10(2/5)
+// initialize prime array with 1, create a number, if that number, prime array shouldn't contain 1!
+// but if created number isn't divisible by another number in the prime array, it itself is prime
+// create number, check against each number in prime array
+
+
+
+// function oddOneOut(str) {
+//     // code me
+//     let splitStr = str.split('');
+//     let dict = [];
+//     return splitStr.map(item => {
+//       if (!splitStr.includes(item) ) {
+//         dict.push[item];
+//       } else {
+
+//       }
+//     });
+//  }
+
+//  console.log( oddOneOut('Hello World') );
+
+// // function sumFibs(num) {
+// //     // okay, just a loop
+// //     function fibSeq(num) {
+// //         let fibColl = [];
+// //         let a = 1, b = 0, temp;
+
+// //         while (num >= 0) {
+// //             temp = a;
+// //             a = a + b;
+// //             fibColl.push(b);
+// //             b = temp;
+// //             num--;
+// //         }
+// //         return fibColl;
+
+// //     }
+
+// //     let fibArr = fibSeq(num);
 
     
-    if (fibArr.length <= 1 ) {
-            return fibArr;
-        } else {
-            return fibArr.filter(item => item <= num && item%2 > 0)
-                        .reduce((result, item) => item + result);
-            } 
+// //     if (fibArr.length <= 1 ) {
+// //             return fibArr;
+// //         } else {
+// //             return fibArr.filter(item => item <= num && item%2 > 0)
+// //                         .reduce((result, item) => item + result);
+// //             } 
 
-   };
+// //    };
 
-console.log( sumFibs(1) ); // reduce error
-console.log( sumFibs(1000) );// works
-console.log( sumFibs(4000000) ); // works with loop, would blow stack with recursion
-console.log( sumFibs(4) ); // works
-console.log( sumFibs(75024) );
-console.log( sumFibs(75025) );
-console.log( sumFibs(10) );
+// // console.log( sumFibs(1) ); // reduce error
+// // console.log( sumFibs(1000) );// works
+// // console.log( sumFibs(4000000) ); // works with loop, would blow stack with recursion
+// // console.log( sumFibs(4) ); // works
+// // console.log( sumFibs(75024) );
+// // console.log( sumFibs(75025) );
+// // console.log( sumFibs(10) );
