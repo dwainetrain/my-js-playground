1d94a64271f2e3c68653832518f65b716545cddf
dwainetrain <dwaine.best@gmail.com>
2020-02-07 06:02:47 -0800

    cleaning up after goole sync mess

--- fcc/tests[Conflict].js
+++ fcc/tests[Conflict].js
@@ -1,52 +1,28 @@
 "use strict";
 
-var Person = function(firstAndLast) {
-  // Complete the method below and implement the others similarly
-  this.getFullName = function() {
-    return "";
-  };
-  return firstAndLast;
-};
+// This is a closure exercise
 
-var bob = new Person('Bob Ross');
-bob.getFullName();
+function addTogether() {
+  for (let i of [...arguments]) {
+  if (typeof i !== 'number') {
+    return undefined;
+    }
+  }
 
-Object.keys(bob).length // should return 6.
-bob instanceof Person // should return true.
-bob.firstName // should return undefined.
-bob.lastName // should //return undefined.
-bob.getFirstName() // should return "Bob".
-bob.getLastName() // should return "Ross".
-bob.getFullName() // should return "Bob Ross".
-bob.getFullName() // should return "Haskell Ross" after bob.setFirstName("Haskell").
-bob.getFullName() // should return "Haskell Curry" after bob.setLastName("Curry").
-bob.getFullName() // should return "Haskell Curry" after bob.setFullName("Haskell Curry").
-bob.getFirstName() // should return "Haskell" after bob.setFullName("Haskell Curry").
-bob.getLastName() // should return "Curry" after bob.setFullName("Haskell Curry").
+  if (arguments.length > 1) {
+    return [...arguments].reduce((a, b) => a + b)
+  }
+  return b => {
+    console.log(typeof b)
+    if (typeof b !== 'number') {
+      return undefined
+    } else {
+      return a + b;
+    }
+} 
+}
 
-
-// // This is a closure exercise
-
-// function addTogether(a) {
-//   for (let i of [...arguments]) {
-//     if (typeof i !== 'number') {
-//       return undefined;
-//       }
-//     }
-
-//   if (arguments.length > 1) {
-//     return [...arguments].reduce((result, item) => result + item)
-//   }
-
-//   return b => {
-//     if (typeof b !== 'number') {
-//       return undefined
-//     } else {
-//       return a + b;
-//     }
-// } 
-// }
-
+console.log(typeof '3')
 
 // let sum2 = addTogether(2);
 // console.log( sum2(2) );
@@ -56,8 +32,7 @@ console.log( '(2,3): ', addTogether(2, 3) ) // should return 5.
 console.log( '(2)(3): ', addTogether(2)(3) )// should return 5.
 console.log( "link: ", addTogether("http://bit.ly/IqT6zt") )// should return undefined.
 console.log( '(2, "3"): ',  addTogether(2, "3") )// should return undefined.
-console.log( '(2)([3]): ', addTogether(2)([3]) )// should return undefined.
-console.log( addTogether(7, 12)(34, 45) );
+console.log( addTogether(2)([3]) )// should return undefined.
 
 // // so if the provided value is either not a property, or the property is false
 // // anywhere in the object, then the whole statement is false
