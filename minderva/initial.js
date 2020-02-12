962ac9ce1b45e8d531a89f9f010976dbf513829e
dwainetrain <dwaine.best@gmail.com>
2020-02-11 17:13:22 -0800

    Minderva initial version

--- minderva/initial.js
+++ minderva/initial.js
@@ -1,47 +1,95 @@
 'use strict';
 
-// Okay, now we need to split up logic
-// So there are different functions within the program
-// One to start the test
-// One to add to entries
-// One to delete entries
-// level of knowledge of card
-
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+// Version 0.01
 
 /* Cards Object */
+// Find some way to protect this number
+let idCount = 3;
+
 let cards = {
-    1 : {'question': 'What is New Mexico\'s Capital?', 'answer':'Santa Fe'},
-    2 : {'question': 'What is Colorado\'s Capital?', 'answer': 'Denver'},
-    3 : {'question': 'What is Wyoming\'s Capital?', 'answer': 'Chyenne'}
+
+    0 : {'question': 'What is New Mexico\'s Capital?', 'answer':'Santa Fe'},
+    1 : {'question': 'What is Colorado\'s Capital?', 'answer': 'Denver'},
+    2 : {'question': 'What is Wyoming\'s Capital?', 'answer': 'Chyenne'},
 }
+/* End Card Object */
 
-// This uses promises to pause node between prompts
-function questionPrompt(q) {
-        
-        return new Promise((resolve, reject) => {
-        rl.question(q, (answer) => {
-            console.log(`You answered: ${answer}`)
-            resolve()
-        })
-        })
+/* Add Card */
+// returns the card id number from objects, to give a unique identifier
+// ideally this will be a protected value
+const cardId = function cardIdNumber() {
+    return idCount;
+};
 
-    }
+// Add card logic
+function addCard(q, a) {
+    const id = cardId();
+    cards[id] = {'question': q, 'answer': a};
+    idCount += 1;
+};
+/* End add card logic */
 
-// use async to go through all cards in collection
-const main = async () => {
+/* Delete Card */
+function deleteCard(id) {
+    delete cards[id];
+    console.log(cards);
+}
+/* End Delete Card*/
+
+/* Edit Card */
+function editCard() {
+    let id = prompt('Which id?');
+    let q = prompt('Enter an updated question');
+    let a = prompt('Enter an updated answer');
+    cards[id] = {'question': q, 'answer': a};
+    console.log(cards);
+}
+/* End Edit Card */
+
+/* Quiz */
+function quiz() {
     for (let card of Object.keys(cards)) {
-        await questionPrompt(cards[card]['question'])
-    };
-  rl.close();
+          prompt(cards[card]['question']);
+          prompt(`The Answer is ${cards[card]['answer']}. Were you correct?`)
+          // some method to keep track of right or wrong answers
+        };
 }
+/* End Quiz */
 
-main()
+/* OLD CODE */
+/* WORKS - allow prompting in node REPL */
+// const readline = require('readline');
 
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
 
+// Question prompt logic
+// This uses promises to pause node between prompts
+// function questionPrompt(q) {
+        
+//         initializePrompt();
+        
+//         return new Promise((resolve, reject) => {
+//         rl.question(q, (answer) => {
+//             console.log(`You answered: ${answer}`)
+//             resolve()
+//         })
+//         })
 
+//     }
+
+// // use async to go through all cards in collection
+// const quiz = async () => {
+//     for (let card of Object.keys(cards)) {
+//         await questionPrompt(cards[card]['question'])
+//     };
+//   rl.close();
+// }
+
+// End of question prompt
+
+// Function to add new question
+// okay, how do you add a unique counter to the object?
+// figure out the function and then add it as a method
