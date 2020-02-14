9b5e5dd221a273bdcf53594439f76fe6fbe3fdac
Science Illustration <scienceillustration@Dwaine-Bests-MacBook-Pro-2.local>
2020-02-14 11:48:22 -0800

    Edits to hamburger css

--- minderva/minderva-v-0.002.js
+++ minderva/minderva-v-0.002.js
@@ -1,38 +1,34 @@
 'use strict';
 
-// NEXT VERSION:
+// THIS VERSION:
 // - it should have a display function in cards object - k
 // - it should have an idCount in cards object - k (note)
 // - it should have an add card method in Cards object - k
 // - it should have a delete card method in Cards object - k
 // - it should have an edit card method in Cards object - k
 // - it should have a quiz method in Cards object -k
+
+// SOMEDAY MAYBE:
+// - it should keep track of right and wrong answers
+// - it should display results at end of session
 // - it should provide answer validation
-// - it should allow yes/no for remembering - ?
 
 /* Cards Object */
-// Find some way to protect this number
-// research ways to make the information private
+// NOTE: Find some way to protect the idCount number
+// research ways to make the property private
 
 let flashCards = {
 
-    // Unique ID
+    // Unique Card Id
     _idCount: 3,
 
-    // Card Collection
-    // Should this be an array of objects?
-    // What would I gain?
-    // cards: {
-    // 0 : {'question': 'What is New Mexico\'s Capital?', 'answer':'Santa Fe'},
-    // 1 : {'question': 'What is Colorado\'s Capital?', 'answer': 'Denver'},
-    // 2 : {'question': 'What is Wyoming\'s Capital?', 'answer': 'Chyenne'},
-    // },
-
+    // Card Collection (contains sample data)
+    // Structure: {uid: int, 'question: string, 'answer': string}
     cards: [
-        {uid: 0, 'question': 'What is New Mexico\'s Capital?', 'answer':'Santa Fe'},
+        {uid: 0, 'question': 'What is New Mexico\'s Capital?', 'answer': 'Santa Fe'},
         {uid: 1, 'question': 'What is Colorado\'s Capital?', 'answer': 'Denver'},
         {uid: 2, 'question': 'What is Wyoming\'s Capital?', 'answer': 'Chyenne'},
-        {uid: 7, 'question': 'What is Wyoming\'s Capital?', 'answer': 'Chyenne'},
+        {uid: 7, 'question': 'What is Britain\'s Capital?', 'answer': 'London'},
     ],
 
     // Display card collection
@@ -47,10 +43,9 @@ let flashCards = {
         return this._idCount;
     },
 
-    checkValidId: function() {
-        if (index === -1) {
-            return false
-        }
+    // Find index based on id of card
+    findIndex: function(id) {
+        return this.cards.findIndex( ({uid}) => uid === id);
     },
 
     // Apply current id to new card, create card, display cards
@@ -63,24 +58,26 @@ let flashCards = {
 
     // Delete Card
     deleteCard: function(id) {
-        let index = this.cards.findIndex( ({uid}) => uid === id);
-        if (index === -1) {
-            return "This card id doesn't exist"
+        let idx = this.findIndex(id);
+        console.log(idx);
+        if (idx !== -1) {
+            this.cards.splice(idx, 1);
+        } else {
+            return 'This card id doesn\'t exit';
         }
-        this.cards.splice(index, 1);
         this.displayCards();
     },
 
     // Edit Card
-    editCard: function() {
-        let id = prompt('Which id?');
-        let q = prompt('Enter and updated question');
-        let a = prompt('Enter an updated answer.');
-        let index = this.cards.findIndex( ({uid}) => uid === id);
-        if (index === -1) {
-            return "This card id doesn't exist"
+    editCard: function(id) {
+        let index = this.findIndex(id);
+        if (index !== -1) {
+            let q = prompt('Enter an updated question');
+            let a = prompt('Enter an updated answer.');
+            this.cards[index] = {uid: id, 'question': q, 'answer': a};
+        } else {
+            return 'This card id doesn\'t exit';
         }
-        this.cards[index] = {'question': q, 'answer': a};
         this.displayCards();
     },
 
@@ -88,8 +85,6 @@ let flashCards = {
         for (let card of Object.keys(this.cards)) {
             prompt(this.cards[card]['question']);
             prompt(`The answer is ${this.cards[card]['answer']}. Were you correct? `)
-            // some method to keep track of right and wrong answers
-            // display results at end of session
         }
-    }
+    },
 }
