bdb0ef50ee86593b2d082737966e82a5b5869a96
dwainetrain <dwaine.best@gmail.com>
2020-03-08 12:43:02 -0700

    Starting design to html/css process, focusing on grid layout.

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {choice, remove} from "./helpers";
+import foods from "./foods"
+class Hello extends React.Component {
+  render(){
+    let fruit = choice(foods)
+    let updateFruits = remove(foods, fruit)
+    return (
+      <div>
+        <p>I'd like a {fruit}, please.</p>
+        <p>Here you go: {fruit}</p>
+        <p>There are {updateFruits} left.</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Hello;
