a937c0c3d96a5c31886b01db74f3dcdca28a53a5
dwainetrain <dwaine.best@gmail.com>
2020-06-05 14:51:43 -0700

    Structered the collected order value into an object of arrays. Next, putting that in local storage.

--- ucsd-javascript/week 7 - final/order.js
+++ ucsd-javascript/week 7 - final/order.js
@@ -5,6 +5,12 @@ It's gonna be a mess!
 // Just a little test data to verify the elasticity of the menu building function
 const testData = { "menu": { "slice of pizza": "2.00", "pizza pie": "25.00", "toppings": { "pepperoni": ".25", "meatballs": ".35", "mushrooms": ".40", "olives": ".20", "vegan":{"brocolli": "3.00"} }, "sides": { "potato salad": "1.25", "hummus": "2.50", "caesar salad": "3.50", "garden salad": "2.25" }, "drinks": { "soda": { "small": "1.95", "medium": "2.20", "large": "2.50" }, "juice": "2.00", "water": "1.25" } }, "Kids menu":{"hamburger":"3.00"}}
 
+/* Food Order Object */
+// Collect item and cost, add up all costs.
+let order = {
+    "item": "cost"
+}
+
 /* Data Retieval */
 const newReq = new XMLHttpRequest();
 newReq.addEventListener("load", function () {
@@ -27,6 +33,7 @@ const menuBuild = (data) => {
     // I tested it adding new items and it seems to work at each level
 
     let container = document.createElement('div')
+    let button = document.createElement('button')
     let h2;
     let figcaption;
     let figure;
@@ -81,14 +88,22 @@ const menuBuild = (data) => {
                             for(item in data[header][subheadOne][subheadTwo]) {
                                 
                                 li = document.createElement('li');
-                                li.className="I'm soda sizes"
-                                li.innerHTML = `${item}, Price: ${data[header][subheadOne][subheadTwo][item]}`;
+                                li.className="soda" // 3rd level item // TODO: Put back in 'item' once you figure this out
+                                // TODO and then add this information to each level of the structure...
+                                let price = data[header][subheadOne][subheadTwo][item]
+                                li.dataset.parent = subheadTwo
+                                li.dataset.item = item
+                                // this is the key, send the structure down the line and use that to return the
+                                // to the next page
+                                li.innerHTML = `${subheadTwo}, ${item}, 
+                                <span class="price" data-price="${price}">Price: ${price},</span> 
+                                <span class="quantity">Quantity: <input type="number" value="0" name="quantity" min="0"></span>`;
                                 ul2.appendChild(li)
                                 
                             }} else { // if clause 2
                                
                                 li = document.createElement('li');
-                                li.className="I'm general items, salad, meatballs, hummus, etc"
+                                li.className="item" // 2nd level item
                                 li.innerHTML = `${subheadTwo}, Price: ${data[header][subheadOne][subheadTwo]}`;
                                 ul.appendChild(li);
                                 figure.appendChild(ul);
@@ -97,7 +112,7 @@ const menuBuild = (data) => {
                 } else { // if clause 1
                     
                     li = document.createElement('li');
-                    li.className="Very Top of Menu Item, ie slice of pizza"
+                    li.className="item" // 1st level item
                     li.innerHTML = `${subheadOne}, Price: ${data[header][subheadOne]}`;
                     ul.appendChild(li);
                     
@@ -105,6 +120,40 @@ const menuBuild = (data) => {
             }
         } 
     }
+    
+    // Setup the button
+    button.setAttribute("name", "submit-order")
+    button.innerHTML = "Review Order"
+    container.appendChild(button)
 
+    // Append Menu
     document.body.appendChild(container)
+
+    // Handle Order Submit
+    document.querySelector('button').addEventListener('click', e => {
+
+        let items = document.querySelectorAll(".soda");
+        items = Array.from(items)
+
+        let selections = items.filter(i => {
+            let qty = i.querySelector("input[name='quantity']").value;
+            return qty > 0
+        })
+            .map(i => {
+            let parent = i.dataset.parent
+            let item = i.dataset.item
+            let price = i.querySelector(".price").dataset.price
+            let qty = i.querySelector("input[name='quantity']").value;
+            return [parent, item, price, qty]
+        })
+
+        let order = {};
+
+        for(i in selections){
+            order[i] = selections[i];
+        }
+
+        console.log(order)
+
+    })
 }
