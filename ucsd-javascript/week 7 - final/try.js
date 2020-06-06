c8676f512fbf0f44efdc829d6f8407faddbbaf73
dwainetrain <dwaine.best@gmail.com>
2020-06-06 08:08:58 -0700

    Tried again, but no luck. Going back to initial plan.

--- ucsd-javascript/week 7 - final/try.js
+++ ucsd-javascript/week 7 - final/try.js
@@ -6,6 +6,12 @@ Code by Dwaine Best
 // Just a little test data to verify the robustness of the menu building function
 const testData = { "menu": { "slice of pizza": "2.00", "pizza pie": "25.00", "toppings": { "pepperoni": ".25", "meatballs": ".35", "mushrooms": ".40", "olives": ".20", "vegan":{"brocolli": "3.00"} }, "sides": { "potato salad": "1.25", "hummus": "2.50", "caesar salad": "3.50", "garden salad": "2.25" }, "drinks": { "soda": { "small": "1.95", "medium": "2.20", "large": "2.50" }, "juice": "2.00", "water": "1.25" } }, "Kids menu":{"hamburger":"3.00"}}
 
+const shortData = { "menu": { "slice of pizza": "2.00", "pizza pie": "25.00", "toppings": { "pepperoni": ".25", "meatballs": ".35", "mushrooms": ".40", "olives": ".20"} }}
+
+const shorterData = { "menu": { "slice of pizza": "2.00", "pizza pie": "25.00"} } // works
+
+const shortestData = { "menu": {"slice of pizza" : "2.00"}} //works
+
 /* Data Retieval */
 const newReq = new XMLHttpRequest();
 newReq.addEventListener("load", function () {
@@ -20,48 +26,56 @@ newReq.addEventListener("error", () => {
 newReq.open("GET", "https://mm214.com/menu.php");
 newReq.send();
 
+//////////////////////////// Works nice enough, but not structured how I would expect...
 /* Build Webpage */
 const menuBuild = (data) => {
-    depth = 0;
-   function header(data) { //working
-       for(i in data) {
-           if(typeof data[i] === 'object'){
-                document.write(`<h3>${depth} ${i}</h3>`)
-                depth++;
-                header(data[i])
-           } else {
-                document.write(`<p>${depth} ${i}</p>`)
-           }
-       }
-   }
-
+//     depth = 0;
+//    function header(data) { //working
+//        for(i in data) {
+//            if(typeof data[i] === 'object'){
+//                 document.write(`<h3>${depth} ${i}</h3>`)
+//                 depth++;
+//                 header(data[i])
+//            } else {
+//                 document.write(`<p>${depth} ${i}</p>`)
+//            }
+//        }
+//    }
+///////////////////////
    function node(data) { //testing
     console.log(data)
     for(i in data) {
         let div = document.createElement('div')
         let h3 = document.createElement('h3')
-        let ul = document.createElement('ul')
-        let li = document.createElement('li')
         
+        let li = document.createElement('li')
+        let ul = document.createElement('ul')
+        console.log("initial node i ", i)
+        console.log("initial node data[i] ", data[i])
+        h3.innerHTML = `header: ${i}`
+        div.appendChild(h3)
 
-        if(typeof data[i] === 'object'){
-            h3.innerHTML = `header: ${i}`
-            div.appendChild(h3)
-            div.appendChild(ul)
+        if(typeof data[i] === 'object'){        
+            
             // body.appendChild(ul)
             console.log("This is node data i", data[i])
             ul.appendChild(item(data[i]))
-            
-       }
-            document.body.appendChild(div)
+            div.appendChild(ul)
         }
+
+        document.body.appendChild(div)
+        
+    }
     }
 
     function item(data) {
         console.log("This is the item data", data)
-        let li = document.createElement('li')
+        let ul = document.createElement('ul')
+        
+        
         for(i in data) {
-            console.log("this is i in data in item", data[i])
+            console.log("this is i in data in item: ", i)
+            let li = document.createElement('li')
             if(typeof data[i] === 'object'){
                 console.log("it's data i:", data[i])
                 li.appendChild(node(data[i]))
@@ -69,9 +83,36 @@ const menuBuild = (data) => {
             //     li.innerHTML = `${i}`
             //     console.log('You made it to item!', li)
             // }
-            li.innerHTML = `Hey, it's the inner html: ${i}`
+            li.innerHTML = `${i}, price: ${data[i]}`
+            ul.appendChild(li)
         }
-        return li
+
+        return ul
     }
-    node(data)
+
+   node(data)
+
+
+//     ///////
+//     function parseNodes(nodes) { // takes a nodes array and turns it into a <ol>
+//         console.log(nodes)
+//         var ol = document.createElement("OL");
+//         for(i in nodes) {
+//             console.log(nodes[i])
+//             ol.appendChild(parseNode(nodes[i]));
+//         }
+//         return ol;
+//     }
+    
+//     function parseNode(node) { // takes a node object and turns it into a <li>
+//         var li = document.createElement("LI");
+//         li.innerHTML = node.title;
+//         li.className = node.class;
+//         console.log(node.nodes)
+//         if(node.nodes) li.appendChild(parseNodes(node.nodes));
+//         return li;
+//     }
+//     /////////
+//     document.body.appendChild(parseNodes(data))
+   
 }
