6b445aab83553a97b2152e741f4840036f1637a6
dwainetrain <dwaine.best@gmail.com>
2020-06-06 10:12:23 -0700

    Class final. Completed to the best of my current abilities.

--- ucsd-javascript/week 7 - final/order copy.js
+++ ucsd-javascript/week 7 - final/order copy.js
@@ -3,6 +3,8 @@ Menu Order Form - UCSD Ext. Javascript Course - Final
 Code by Dwaine Best
 */
 
+// Add a save order button and retrieve order button
+
 // Just a little test data to verify the robustness of the menu building function
 const testData = { "menu": { "slice of pizza": "2.00", "pizza pie": "25.00", "toppings": { "pepperoni": ".25", "meatballs": ".35", "mushrooms": ".40", "olives": ".20", "vegan":{"brocolli": "3.00"} }, "sides": { "potato salad": "1.25", "hummus": "2.50", "caesar salad": "3.50", "garden salad": "2.25" }, "drinks": { "soda": { "small": "1.95", "medium": "2.20", "large": "2.50" }, "juice": "2.00", "water": "1.25" } }, "Kids menu":{"hamburger":"3.00"}}
 
@@ -24,19 +26,18 @@ newReq.send();
 const menuBuild = (data) => {
     // Many affordances had to be made due to the structure of the JSON
     // I tried plenty of algorythmic ways to approach it
-    // but this seemed to work best for the data structure's hierarchy
+    // but this seemed to work  best for data structure
     // I tested it adding new items and it seems to work on any data up to this depth
 
     /*
     Also,
     I spent an exorbitant amount of time on structuring the html of this
     only to find out that figure and figcaption don't validate when nested. CURSES!!!
-    Lesson Learned, test validity first!
-    I'll save the valid html restructuring for Version 2.0
+     Well, I'll save the valid html restructuring for Version 2.0
     */
 
     let container = document.createElement('div')
-    
+    let button = document.createElement('button')
     let h2;
     let figcaption;
     let figure;
@@ -61,8 +62,8 @@ const menuBuild = (data) => {
                 ul = document.createElement('ul');
                 ul2 = document.createElement('ul');
                 // assign classes
-                ul.className = "first-level-list"
-                ul2.className="second-level-list"
+                ul.className = "first level list"
+                ul2.className="second level list"
                 // insert data
                 figcaption.innerHTML = subheadOne;
                 // append nodes
@@ -75,7 +76,7 @@ const menuBuild = (data) => {
                             figureTwo = document.createElement('figure');
                             figcaption = document.createElement('figcaption');
                             // assign classes
-                            figureTwo.className = "figure-two";
+                            figureTwo.className = "Figure Two";
                             // insert data
                             figcaption.innerHTML = subheadTwo;
                             // append nodes
@@ -99,7 +100,7 @@ const menuBuild = (data) => {
                             }} else { // if clause 2
                                
                                 li = document.createElement('li');
-                                li.className="item" // 2nd level item // ie. toppings and sides
+                                li.className="item" // 2nd level item // toppings and sides
                                 let price = data[header][subheadOne][subheadTwo]
                                 li.dataset.parent = subheadOne
                                 li.dataset.item = subheadTwo
@@ -114,7 +115,7 @@ const menuBuild = (data) => {
                 } else { // if clause 1
                     
                     li = document.createElement('li');
-                    li.className="item" // 1st level item // ie. slice of pizza
+                    li.className="item" // 1st level item
 
                     let price = data[header][subheadOne]
                     li.dataset.parent = header
@@ -132,13 +133,11 @@ const menuBuild = (data) => {
     }
     
     // Setup the save order button
-    let saveButton = document.createElement('button')
     saveButton.setAttribute("name", "save-order")
     saveButton.innerHTML = "Save Order"
     container.appendChild(saveButton)
 
-    // Setup the review order button
-    let button = document.createElement('button')
+    // Setup the button
     button.setAttribute("name", "submit-order")
     button.innerHTML = "Print Order"
     container.appendChild(button)
@@ -146,8 +145,8 @@ const menuBuild = (data) => {
     // Append Menu
     document.body.appendChild(container)
 
-    // Handle Order Save
-    document.querySelector("button[name='save-order']").addEventListener('click', e => {
+    // Handle Order Submit
+    document.querySelector('savebBtton').addEventListener('click', e => {
 
         // get all items
         let items = document.querySelectorAll(".item");
@@ -174,12 +173,9 @@ const menuBuild = (data) => {
             order[i] = selections[i];
         }
 
-        // Store order object in local storage
+        // Store order in local storage
         localStorage.setItem('customerOrder', JSON.stringify(order))
-    })
-    
-    // Handle Order Review
-    document.querySelector("button[name='submit-order']").addEventListener('click', e => {
+
         // Retrieve from local stoage and display to customer
         getOrder = JSON.parse(localStorage.getItem('customerOrder'));
 
@@ -193,10 +189,7 @@ const menuBuild = (data) => {
             p.innerHTML = `
             <strong>${item[0]}, ${item[1]}</strong>, 
             Price: $${item[2]} x ${item[3]} = $${itemTotal}`
-            
-            // keep running total
             total = total + itemTotal;
-            
             div.appendChild(p)
         }
 
@@ -233,5 +226,3 @@ const menuBuild = (data) => {
 
     })
 }
-
-console.log("Thank you, Kris! This has been an excellent challenge!")
