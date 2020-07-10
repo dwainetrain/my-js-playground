99b8a5689432531a4d0065908d3890424fbcc2d1
dwainetrain <dwaine.best@gmail.com>
2020-07-10 09:30:20 -0700

    Week 3, jsx and reactstrap implemented. Html updated to bootstrap4.

---
+++ ucsd-applied-js/week-3/navbar-jsx/src/Content.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Content extends React.Component {
+    render(){
+        return(
+            <div className="container">
+                <h3>Basic Navbar Example</h3>
+                <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
+            </div>
+        )
+    }
+}
+
+export default Content;
---
+++ ucsd-applied-js/week-3/navbar-jsx/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render() {
+        return(
+            <h2>Hello from Footer!</h2>
+        )
+    }
+}
+
+export default Footer;
