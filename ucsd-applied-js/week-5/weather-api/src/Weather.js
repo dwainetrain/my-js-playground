ddf9112c7bdad63bd0a059a8e30cf42e0bd3fe25
dwainetrain <dwaine.best@gmail.com>
2020-07-21 05:57:50 -0700

    Week 5 started and Delete added to Alpha Minderva

---
+++ ucsd-applied-js/week-5/weather-api/src/Weather.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Weather extends Component{
+
+    constructor(){
+        super();
+        this.state = { data:{main:{temp:''}} }
+    }
+
+    // The basic structure 
+    async componentDidMount() {
+        const response = await fetch("https://mm214.com/demo.php")
+        const json = await response.json()
+        this.setState({ data: json })
+        // console.log(this.state.data.main.temp)
+    }
+
+   
+
+    render(){
+        const temp = this.state.data.main.temp
+
+        return(
+            <div>
+                <p>The temperature is {temp}</p>
+            </div>
+        )
+    }
+
+}
+
+export default Weather;
