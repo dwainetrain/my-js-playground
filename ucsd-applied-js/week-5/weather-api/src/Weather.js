912846e0074eec41b9d7aa9d724326ca61ce503e
dwainetrain <dwaine.best@gmail.com>
2020-07-22 08:10:20 -0700

    More component splitting and routing. Stuck on getting edit link to work. More Reach Router docs reading is in order.

--- ucsd-applied-js/week-5/weather-api/src/Weather.js
+++ ucsd-applied-js/week-5/weather-api/src/Weather.js
@@ -7,15 +7,15 @@ class Weather extends Component{
         this.state = { data:{main:{temp:''}} }
     }
 
-    // The basic structure 
+    // The basic structure
+    // pull other information from your assignment last quarter...
+    // add a choose your own city api
     async componentDidMount() {
         const response = await fetch("https://mm214.com/demo.php")
         const json = await response.json()
         this.setState({ data: json })
         // console.log(this.state.data.main.temp)
     }
-
-   
 
     render(){
         const temp = this.state.data.main.temp
